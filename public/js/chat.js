40f55c7383c0fc45f38723900cde8f5d256de95a
IonitaCatalin <ionita.catalin2000@gmail.com>
2021-12-03 15:06:32 +0200

    made everythingn definetely beautiful

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,7 @@
-let socket = io();
+const socket = io();
 
-let content = document.querySelector('.conversation-wrapper');
-let message = document.querySelector('#message-text');
-let submit = document.querySelector('.send');
+const content = document.querySelector('.conversation-wrapper');
+const message = document.querySelector('#message-text');
 
 socket.on('NEW_MESSAGE', (message) =>{
 
@@ -29,41 +28,124 @@ socket.on('NEW_MESSAGE', (message) =>{
 
     content.appendChild(messageReceived);
 
-    
+});
+
+socket.on('SEND_LOCATION',({latitude, longitude}) => {
+    let messageLocation = document.createElement('div');
+    let messageLocationText = document.createElement('div');
+
+    messageLocation.className = 'message-location';
+    messageLocationText.className = 'location';
+
+    messageLocationText.innerHTML = `🌎 An user is at https://google.com/maps?q=${latitude},${longitude}, go say hi!`;
+    messageLocation.appendChild(messageLocationText);
+
+    content.appendChild(messageLocation);
+
 })
 
-submit.addEventListener('click', function (event) {
+document.querySelector('.send').addEventListener('click', (event) => {
 
     event.preventDefault();
 
-    socket.emit('SEND_MESSAGE',(message.value));
 
     let messageSent = document.createElement('div');
     let messageSentBubble = document.createElement('div');
     let messageSentTime = document.createElement('div');
     let messageSentAvatar = document.createElement('div');
     let messageSentTimestamp = document.createElement('div');
+    let messageSentStatus = document.createElement('div');
+
 
     messageSent.className = 'message-sent';
     messageSentBubble.className = 'bubble';
     messageSentTime.className = 'time';
     messageSentTimestamp.className = 'timestamp';
     messageSentAvatar.className = 'avatar';
+    messageSentStatus.className = 'status';
+
+
 
     messageSentBubble.innerHTML = message.value;
+
+    messageSentStatus.innerHTML = '⌛';
     messageSentAvatar.innerHTML = 'ME';
     messageSentTime.innerHTML = new Date().getHours() + ':' + new Date().getMinutes();
 
 
+    messageSentTime.appendChild(messageSentStatus);
     messageSentTimestamp.appendChild(messageSentTime);
     messageSentTimestamp.appendChild(messageSentAvatar);
+
+
     messageSent.appendChild(messageSentBubble);
     messageSent.appendChild(messageSentTimestamp); 
 
     content.appendChild(messageSent);
 
+    socket.emit('SEND_MESSAGE',message.value, ({timestamp,acknowledged}) => {
+
+        console.log(timestamp);
+
+        if(acknowledged) {
+            messageSentStatus.innerHTML = '⌛';
+        } else {
+            messageSentStatus.innerHTML = '❌';
+        }
+        setTimeout(() => {
+            messageSentStatus.innerHTML = '❌';
+            return;
+        },50000)
+    }); 
+
     message.value = '';
 
+})
 
+document.querySelector('.location').addEventListener('click', (event) => {
+
+    event.preventDefault();
+
+    if(!navigator.geolocation) {
+        return alert('Geolocation API is not supported by your browser!');
+    }
+
+
+
+    let messageLocation = document.createElement('div');
+    let messageLocationText = document.createElement('div');
+
+    messageLocation.className = 'message-location';
+    messageLocationText.className = 'location';
+
+    messageLocationText.innerHTML = `🌎 We are determining your location ⌛ !`;
+    messageLocation.appendChild(messageLocationText);
+
+    content.appendChild(messageLocation);
+
+    
+
+    navigator.geolocation.getCurrentPosition((position) => {
+
+        const {latitude,longitude} = position.coords;
+
+        messageLocationText.innerHTML = `🌎 You shared your location at https://google.com/maps?q=${latitude},${longitude} !`;
+
+        socket.emit('SHARE_LOCATION',{latitude, longitude}, ({timestamp, acknowledged}) => {
+
+            
+            if(acknowledged) {
+                messageLocationText.innerHTML += '✅';
+            } else {
+                messageLocationText.innerHTML += '❌';
+            }
+            
+            setTimeout(() => {
+                messageLocationText.innerHTML += '❌';
+                return;
+            },50000)
+
+        }); 
+    })
 
 })
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const sockets = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,14 +19,29 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('USER_JOINED','A new user has joined!');
 
-    socket.on('SEND_MESSAGE', (message) => {
+    socket.on('SEND_MESSAGE', (message, callback) => {
+
+        const filter = new Filter();
+
+        if(filter.isProfane(message)) {
+            callback({timestamp: Date.now(),acknowledged: false});
+        }
+
+        callback({timestamp: Date.now(),acknowledged: true});
         socket.broadcast.emit('NEW_MESSAGE', message);
+
     });
 
     socket.on('disconnect', () => {
         io.emit('USER_LEFT','A user has disconnected!');
     })
 
+    socket.on('SHARE_LOCATION', ({latitude, longitude}, callback) => {
+
+        callback({timestamp: Date.now(),acknowledged: true});
+        socket.broadcast.emit('SEND_LOCATION', ({latitude, longitude}));
+    })
+    
 })
 
 server.listen(port ,() => {    
